8eefad535d970f811545da439c215d2aad5722e3
Arthur Simoes <103394351+ThurSimoes@users.noreply.github.com>
2025-04-01 09:43:19 -0300

    Create Carta_do_acre.c

---
+++ Carta_do_acre.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() { 
+    char estado[] = "Acre";
+    char codigo_da_carta[] = "A02";
+    char nome_da_cidade[] = "Rio Branco";
+    int populacao = 830018; /* população aproximada */
+    float area_km = 152581.0; /* área em km² */
+    float pib = 23.6; /* PIB em milhões de reais */
+    int numeros_de_pontos_turisticos = 10; /* número de pontos turísticos */
+
+    return 0;
+}
